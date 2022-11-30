2d825916523ba2269e70910ee4042927352fd3d9
sasloan <ssloan15@msudenver.edu>
2022-11-30 14:19:56 -0700

    Complete problem 2b

--- project 2.py
+++ project 2.py
@@ -28,7 +28,95 @@ def countWords(paramString):
     return stringLength
 
 print(f"The number of words in your Sentance is: {countWords(userSentence)}")
+print("")
     
     
+# Problem 2a
+
+print(Header('2a'))
+print("")      
+print("")      
       
+print("""For this Program we will be taking in a associates salary and the percentage \n
+      of the salary they earned to determine what their annual bonus should be \n
+      you will need to input both the associates salary and what your companies percentage \n
+      for bonus's is to determine how much this individuals bonus pay will be. Please \n
+      only enter only numbers into this program as anything else will crash the program, thank you!""")
       
+associateSalary = input("Please input the associates GROSS annual income here: $")
+
+companyBonusPercentage = input("Please input the companies percentage for bonus here: ")
+
+
+def findBonus(paramSalary,paramBonusPercent):
+    
+    convertPercent = float(paramBonusPercent)/100
+    
+    bonusAmount = float(paramSalary) * float(convertPercent)
+    
+    return bonusAmount
+
+print(f"The bonus amount for this associate is:${findBonus(associateSalary,companyBonusPercentage)}")
+print("")
+
+# Problem 2b
+
+print(Header('2b'))
+print("")
+print("")
+
+print("""For this Program you will be a HR manager entering in bonuses for your associates \n
+      So first you will enter the associates 'employee ID' then the associates 'Annual Salary' \n
+      , then the companies 'bonus percentage' for that associate. Then the system will output \n
+      the associates employee ID, Annual Salary, Bonus percentage, bonus amount, How many associates \n
+      there are, The total bonuses issued and finally the average bonuses issued.\n
+      To end the program to get your outputs please type in "quit" for the \n
+      employee ID section. Please only enter numbers as anything else you enter \n
+      will crash the program, thank you! """)
+print("")
+print("")
+print("")
+
+assRow = []
+
+count = 0
+
+totalBonusesIssued = 0
+
+assEmployeeID = input("Please input the Associates 'Employee ID' here: ")
+
+while(assEmployeeID != "quit"):
+ 
+    assRow.append(assEmployeeID)
+     
+    assSalary = input("Please input the Associates Annual Salary here: $")
+    
+    assRow.append(assSalary)
+    
+    assBonusPercentage = input("Please input the Associates Annual Bonus Percentage here: ")
+    
+    assRow.append(assBonusPercentage)
+    
+    assBonus = findBonus(float(assSalary),float(assBonusPercentage))
+    
+    assRow.append(assBonus)
+    
+    count = count + 1
+    
+    totalBonusesIssued = totalBonusesIssued + assBonus
+    
+    avgAssBonus = totalBonusesIssued/count
+
+    print(f"""\n{assRow}""")    
+  
+    assEmployeeID = input("Please input the Associates 'Employee ID' here: ")
+       
+else:
+    
+    assRow.append(count)
+   
+    assRow.append(totalBonusesIssued)
+    
+    assRow.append(avgAssBonus)
+
+print(assRow)
